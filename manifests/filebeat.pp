f0d059385b5d3bd33cecd6919827e8d11a7e03f2
iDhruven <35227460+iDhruven@users.noreply.github.com>
2021-03-01 16:53:06 -0800

    Adding Prospectors

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -4,7 +4,20 @@
 #
 # @example
 #   include elk::filebeat
+
+# Filebeat prospectors - expects array of structured data
+# [ {
+#   "type" => "log",
+#   "paths" => [
+#      "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
+#      "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
+#    ],
+#   "json_keys_under_root" => true
+# } ] 
+
+
 class elk::filebeat (
+	$prospectors = [],
 	$logstash_server = '127.0.0.1',
 	$logstash_port   = '5044' 
 ){
@@ -21,6 +34,7 @@ class elk::filebeat (
 	file {'/etc/filebeat/filebeat.yml':
 		ensure => file,
 		content => epp('elk/filebeat.yml.epp'),{
+			prospectors	=> $prospectors,
 			logstash_server => $logstash_server,
 			logstash_port	=> $logstash_port
 			})
